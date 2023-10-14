e66965e35d931186cae5eef35f2aed05c4cc1b2a
LasayaLS <llasaya@mail.ru>
2023-10-14 14:05:28 +0300

    задание 4 fix2

--- task_4/solution.js
+++ task_4/solution.js
@@ -18,6 +18,6 @@ function renderProductsCards(json) {   // Напишите функцию render
    let products = parseProducts(json);   // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
    let length = products.length;   // Запишите в переменную length значение свойства products.length
    for (let i = 0; i < length; i +=1) {   // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-      addProduct(products);   // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+      addProduct(products[i]);   // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
    }
 }
